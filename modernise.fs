71f592ef387c11f52ab6f398974fc96b540b8c29
francois pussault <goblinrieur@gmail.com>
2023-06-21 18:38:29 +0200

    at-xy function // drops stored same place & few new comments

--- modernise.fs
+++ modernise.fs
@@ -6,51 +6,42 @@ variable 'src-fd-in
 variable tobeat 									\ to know if new high score is reached
 variable fd-in
 0 value fid1
+\ words
 : COLORIZE ( n -- ) 
 	\ just like terminal ascii \e[31m, here use 31 colorize 
 	ESC EMIT ." [" base @ >R 
 	0 <# #S #> type 
 	R> base ! ." m"  
 ; 
-
 \ Forth words for HIRES-mode graphics
 \ given these dialect-specific words an "adaptor shim" could be made for any other Forth
 : hcolour ( colour ---) drop ;	( select current colour for HIRES drawing mode)
 : hline ( x y --- ) 2drop ;	( draw HIRES mode line to position)
 : hposn ( x y --- ) 2drop ;	( moves HIRES mode pixel cursor)
 : scale ( scale --- ) drop ;	( sets shape table scaling value)
-
+: draw ( addr delim --- ) ( draw shape table, presumably first value is address and second is delimiter)
+   2drop  ( discard values from stack)
+;
+\ as far as it is only drops I would choose between keeping it for historic & replace them all in main code : keep it :) 
 \ implement home, hclr and vhtab using ANSI commands
 \ SEE: https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
-
 : home ( --- ) ( set cursor to home position, using ANSI codes)
 	.\" \e[H" 
 ;
-
 \ real HCLR would clear HIRES1
 : hclr ( --- ) ( clear screen using ANSI codes)
 	page
 ;
-
+\
 : vhtab ( y x --- ) ( position cursor on screen, using ANSI codes)
-	swap .\" \e[" 
-	0 <# #s #> type
-	." ;"
-	0 <# #s #> type ." H" 
+	swap at-xy
 ;
-
-: draw ( addr delim --- ) ( draw shape table, presumably first value is address and second is delimiter)
-   2drop  ( discard values from stack)
-;
-
 ( POSSIBLE FIG WORDS NOT SUPPORTED IN ANS FORTH)
 \ HELPFUL RESOURCE: https://dwheeler.com/6502/fig-forth-glossary.txt
 \ Helpful book: Forth Fundamentals Vol. 2, C. Kevin McCabe, dilithium Press 1983, ISBN 0-88056-092-4
 : MINUS negate	 ( FFv2 page 97) ;
 : -DUP ?dup	 ( FFv2 page 39) ;
-
 \ this woudln't be a problem on the Apple ][ but on modern systems the KEY routine is case sensitive
-
 : inkey ( --- key)
    key dup dup
    96 >
@@ -61,7 +52,7 @@ variable fd-in
             endif
      endif
 ;    
-
+\ not really needed as far as I guess noone uses versions of gforth older than 0.7.x
 : checkversion ( -- exit|continue ) 
 	\ check gforth version 
 	version-string 2 - s\" 0.7" str= 0 >= if 	\ easy method to get tooling version
@@ -79,13 +70,11 @@ variable fd-in
 : readfile
 	here 'src-fd-in ! 							\ ram position
 	fname count r/o open-file throw fd-in !
-	\ s" gamedata/.score" r/o open-file throw fd-in !
 	here 16 fd-in @ read-file throw 
 	dup allot								\ one alloc = 1 line
 	fd-in @ close-file throw						\ now close file
 	here 'src-fd-in @ - #src-fd-in ! 					\ get allocated
 	'src-fd-in @ #src-fd-in @ s>number drop tobeat ! 
-
 ;
 : highscore? ( finalscore > fd-in -- file )
 	tobeat @  score @  < if
